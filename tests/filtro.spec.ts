5c903ba91a4e480732d2a8ab2681a3ff004b8f40
Charles Clezar Pereira <103696544+CharlesClezar@users.noreply.github.com>
2025-06-25 20:34:48 -0300

    Delete tests directory

--- tests/filtro.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:8080/');
-  await page.getByRole('textbox', { name: 'Data Início' }).fill('2022-01-01');
-  await page.getByRole('textbox', { name: 'Data Fim' }).fill('2022-12-31');
-  await page.getByRole('button', { name: 'Aplicar Filtros' }).click();
-  await page.getByText('Nenhum dado disponível para').click();
-});
